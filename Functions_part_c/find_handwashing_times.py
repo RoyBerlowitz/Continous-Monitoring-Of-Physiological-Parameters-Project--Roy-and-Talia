8bd810478e81b50a258c88dc0ba3459a8c61487b
RoyBerlowitz <roee.berlo.21@gmail.com>
2026-01-04 13:35:01 +0200

    final for today

--- Functions_part_c/find_handwashing_times.py
+++ Functions_part_c/find_handwashing_times.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from .window_timing_translator_preprocessing import get_handwashing_times
+from .window_timing_translator_preprocessing import get_handwashing_times, apply_smoothing
 from .timing_classifying_without_model import translate_prediction_into_time_point_prediction_with_weights, train_for_decision
 
 # חשוב ליצור לפני זה את הclassification df גם לטסט וגם לטריין כולל הפרדיקשן - זה שלב מקדים!!!
@@ -14,8 +14,11 @@ def predict_times(train_df, test_df, data_files, classification_flag = "model", 
         train_x, train_y = translate_prediction_into_time_point_prediction_with_weights (train_df, weight_flag)
         y_probs = train_x["weighted_prob"]
         test_x, test_y = translate_prediction_into_time_point_prediction_with_weights (test_df, weight_flag)
-        threshold_no_median, threshold_with_median = train_for_decision(train_x, train_y, group_indicator =train_x["Group number"] , n_iteration=50, n_jobs=-1)
+        threshold_no_median, threshold_with_median, filter_size = train_for_decision(train_x, train_y, group_indicator =train_x["Group number"] , n_iteration=50, n_jobs=-1)
         pred_y_no_median_filter =  (y_probs >= threshold_no_median).astype(int)
         pred_y_with_median_filter =  (y_probs >= threshold_with_median).astype(int)
+        smoothed_prediction = apply_smoothing(pred_y_with_median_filter, filter_size)
+        # pred_y_no_median_filter, smoothed_prediction are our predictions - צריך להכין פונקצית אבליואציה למול הלייבלים האמיתיים ולקבל גם אולי סיווג לפי שניות ממש
+
         # כאן צריך להוסיף מטריקות אבליואציה וכו'
 
