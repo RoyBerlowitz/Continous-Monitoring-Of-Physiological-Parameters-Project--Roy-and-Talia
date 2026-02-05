e97c2cce8cd34290117015c98345745cbe0b0d96
talia <taliaheching@gmail.com>
2026-02-05 14:34:57 +0200

    added final train thing

---
+++ 02_train/02_train.py
@@ -0,0 +1,181 @@
+from pathlib import Path
+import time
+
+from Functions_train import *
+
+def run_train(save_cache=False, recompute_functions=RecomputeFunctionsConfig(), group_name = "02"):
+    start_time = time.time()
+
+    window_models = [WindowModelNames.XGBOOST] #talia
+    # window_models = [WindowModelNames.RANDOM_FOREST] #roee
+
+    second_models = [SecondModelNames.NO_MODEL]#, SecondModelNames.MARKOV] #SecondModelNames.LOGISTIC, decided not to use
+
+    ## ========================================================================================================== ##
+    ##                                               PREPROCESSING                                                ##
+    ## ========================================================================================================== ##
+    ## ==================================== Load Data ==================================== ##
+    data_path = Path(__file__).resolve().parent.parent / "data"
+    data_files = load_cache(
+        "load_data.pkl",
+        lambda: load_data(data_path),
+        force_recompute=recompute_functions.load_data,
+        save=save_cache
+    )
+    print('\033[32mData loaded\033[0m')
+
+    ## ==================================== Segmentation ==================================== ##
+    X_matrix, y_train = load_cache(
+        "segment_signal.pkl",
+        lambda: segment_signal(7, 0.25 * 7, data_files), #params were chosen in Part A by maximizing MU
+        force_recompute=recompute_functions.segment_signal,
+        save=save_cache
+    )
+    print('\033[32mSegmentation completed\033[0m')
+
+    ## ========================================================================================================== ##
+    ##                                                FEATURES                                                    ##
+    ## ========================================================================================================== ##
+
+    ## ==================================== Feature Extraction ==================================== ##
+    X_train = load_cache(
+        "extract_features.pkl",
+        lambda: extract_features(X_matrix, data_files),
+        force_recompute=recompute_functions.extract_features,
+        save=save_cache
+    )
+    print('\033[32mFeature extraction completed\033[0m')
+    print((X_train['Group number']+X_train['Participant ID']).unique()) #!TODO
+
+    # # ## ==================================== CNN Embedding ==================================== ##
+    X_train = load_cache(
+        "cnn_embedding.pkl",
+        lambda: cnn_embedding_full_workflow(X_train, y_train, group_name, test_flag=False),
+        force_recompute=recompute_functions.cnn_embedding,
+        save=save_cache
+    )
+    print('\033[32mCNN embedding completed\033[0m')
+
+    ## ==================================== Normalization ==================================== ##
+    [X_train, scaler] = load_cache(
+        "feature_normalization.pkl",
+        lambda: normalize_train(X_train),
+        force_recompute=recompute_functions.feature_normalization,
+        save=save_cache
+    )
+    save_pickle_to_test(scaler, "normalization_train_scaler.pkl")
+    print('\033[32mFeature normalization completed\033[0m')
+
+    ## ==================================== Vetting ==================================== ##
+    chosen_vet_features = load_cache(
+        "vet_features.pkl",
+        lambda: vet_features(X_train, y_train),
+        force_recompute=recompute_functions.vet_features,
+        save=save_cache
+    )
+    X_train = X_train[chosen_vet_features]
+    print('\033[32mFeature vetting completed\033[0m')
+
+    ## ========================================================================================================== ##
+    ##                                               WINDOW MODELS                                                ##
+    ## ========================================================================================================== ##
+
+    trained_window_models = {}
+    trained_second_models = {}
+    X_train_seconds_dfs = {}
+
+    for window_model in window_models:
+        trained_second_models[window_model] = {}
+        ## ==================================== Wrapper Feature Selection ==================================== ##
+        [selected_feats,_] = load_cache(
+            f"select_features_{window_model}.pkl",
+            lambda: select_features(X_train, y_train, models_hp_for_wrapper[window_model], split_by_group_flag=True),
+            force_recompute=recompute_functions.select_features,
+            save=save_cache
+        )
+        save_pickle_to_test(selected_feats, f"select_features_{window_model}_train.pkl")
+        print(f'\033[32mFeature selection completed model: {window_model}\033[0m')
+
+        X_selected = X_train[selected_feats + admin_features]
+
+        ## ==================================== Choose Best HP For Window Models ==================================== ##
+        model_best_hp = load_cache(
+            f"choose_hyperparameters_{window_model}.pkl",
+            lambda: choose_hyperparameters(X_selected, y_train, window_model, n_iterations=50, split_by_group_flag=True),
+            force_recompute=recompute_functions.choose_hyperparameters,
+            save=save_cache
+        )
+        print(f'\033[32mChoose hp completed model: {window_model}\033[0m')
+
+        ## ==================================== Train Window Models ==================================== ##
+        trained_window_models[window_model], X_train_seconds_dfs[window_model] = load_cache(
+            f"train_window_model_{window_model}.pkl",
+            lambda: train_window_model(X_selected, y_train, model_best_hp, window_model, split_by_group_flag=True),
+            force_recompute=recompute_functions.train_window_model,
+            save=save_cache
+        )
+        save_model_to_test(trained_window_models[window_model], f"train_window_model_{window_model}_train.joblib")
+        print(f'\033[32mFinished training model: {window_model}\033[0m')
+
+        # # ========================================================================================================== ##
+        # #                                                  SECOND MODELS                                             ##
+        # # ========================================================================================================== ##
+        for second_model in second_models:
+            ## ==================================== Train Second Models ==================================== ##
+            trained_second_models[window_model][second_model] = load_cache(
+                f"train_second_model_{window_model}_{second_model}.pkl",
+                lambda: prediction_by_second_train(X_train_seconds_dfs[window_model], data_files, window_model, second_model),
+                force_recompute=recompute_functions.train_second_model,
+                save=save_cache
+            )
+            save_pickle_to_test(trained_second_models[window_model][second_model], f"train_second_model_{window_model}_{second_model}_train.pkl")
+            print(f'\033[32mFinished training second model: {window_model}-{second_model}\033[0m')
+
+    # # ========================================================================================================== ##
+    # #                                               EVALUATE MODELS                                              ##
+    # # ========================================================================================================== ##
+    # for window_model in window_models:
+    #     model_stats = {}
+    #     model_stats['window'] = load_cache(
+    #         f"evaluate_window_model_{window_model}.pkl",
+    #         lambda: evaluate_one_model(trained_window_models[window_model], window_model, X_selected, y_train),
+    #         force_recompute=recompute_functions.evaluate_models,
+    #         save=save_cache
+    #     )
+    #     print(f'\033[32mFinished evaluating model: {window_model}\033[0m')
+    #
+    #     for second_model in second_models:
+    #         model_stats[second_model] = load_cache(
+    #             f"evaluate_second_model_{window_model}_{second_model}.pkl",
+    #             lambda: prediction_by_second_test(X_train_seconds_dfs[window_model], data_files, window_model, trained_second_models[window_model][second_model], second_model),
+    #             force_recompute=recompute_functions.evaluate_models,
+    #             save=save_cache
+    #         )
+    #         print(f'\033[32mFinished evaluating second model: {window_model}-{second_model}\033[0m')
+    #
+    #     save_second_model_stats(model_stats, window_model)
+
+
+    end_time = time.time()
+    print(f"Total time: {end_time - start_time} sec")
+
+    return
+
+# ========================================================= Run =========================================================
+if __name__ == "__main__":
+
+    recompute_functions = RecomputeFunctionsConfig(
+        load_data=False,
+        segment_signal=False,
+        extract_features=False,
+        cnn_embedding=False,
+        feature_normalization=False,
+        vet_features=False,
+        select_features=False,
+        choose_hyperparameters=False,
+        train_window_model=False,
+        create_test_time_df=False,
+        train_second_model=False,
+        evaluate_models=False,
+    )
+    run_train(save_cache=True, recompute_functions=recompute_functions)
---
+++ 02_train/Functions_train/Features/__init__.py
@@ -0,0 +1,4 @@
+from .vet_features import normalize_train, normalize_test, vet_features
+from .cnn_embedding import cnn_embedding_full_workflow
+from .extract_features import extract_features
+from .split_data import split_data
