823043ee2c66a0c7c5236bc73e7155eca59879c1
RoyBerlowitz <roee.berlo.21@gmail.com>
2026-02-05 12:40:16 +0200

    organization

--- o02_train/o02_train.py
+++ o02_train/o02_train.py
@@ -52,16 +52,15 @@ def run_train(save_cache=False, recompute_functions=RecomputeFunctionsConfig(), 
         print('Train Getting data from func params')
 
     # ## ==================================== CNN Embedding ==================================== ##
-    #informative_features = ['cnn_emb_2', 'cnn_emb_6', 'cnn_emb_5', 'Gyro_Y-AXIS_dominant_frequency', 'Gyro_Y-AXIS_CUSUM-_Feature', 'Acc_X-AXIS_acceleration_std', 'Acc_X_Z_CORR', 'Gyro_SM_velocity_median', 'Gyro_Y-AXIS_CUSUM+_Feature', 'Acc_SM_dominant_frequency', 'Mag_Y-AXIS_median', 'Gyro_Z-AXIS_band_to_tot_energy_ratio', 'Mag_MEAN_AXES_CORR', 'cnn_emb_8', 'Acc_SM_acceleration_median', 'Acc_X-AXIS_velocity_skewness', 'Acc_Z-AXIS_velocity_median', 'Acc_SM_frequency_centroid', 'Acc_Z-AXIS_CUSUM+_Feature', 'Gyro_Y-AXIS_velocity_median']
-    informative_features = [ 'Gyro_Y-AXIS_dominant_frequency', 'Gyro_Y-AXIS_CUSUM-_Feature', 'Acc_X-AXIS_acceleration_std', 'Acc_X_Z_CORR', 'Gyro_SM_velocity_median', 'Gyro_Y-AXIS_CUSUM+_Feature', 'Acc_SM_dominant_frequency', 'Mag_Y-AXIS_median', 'Gyro_Z-AXIS_band_to_tot_energy_ratio', 'Mag_MEAN_AXES_CORR', 'Acc_SM_acceleration_median', 'Acc_X-AXIS_velocity_skewness', 'Acc_Z-AXIS_velocity_median', 'Acc_SM_frequency_centroid', 'Acc_Z-AXIS_CUSUM+_Feature', 'Gyro_Y-AXIS_velocity_median']
-    X_train, chosen_features = load_cache(
+
+    X_train = load_cache(
         "cnn_embedding.pkl",
-        lambda: cnn_embedding_full_workflow(X_train, y_train, informative_features, group_name),
+        lambda: cnn_embedding_full_workflow(X_train, y_train, group_name, test_flag=False),
         force_recompute=recompute_functions.cnn_embedding,
         save=save_cache
     )
+
     print('\033[32mCNN embedding completed\033[0m')
-    print(chosen_features)
     # valid_columns = [c for c in informative_features + admin_features if c in X_train.columns]
     # X_train = X_train[valid_columns]
 
@@ -76,20 +75,16 @@ def run_train(save_cache=False, recompute_functions=RecomputeFunctionsConfig(), 
     print('\033[32mFeature normalization completed\033[0m')
 
     ## ==================================== Vetting ==================================== ##
-    # The reason the vetting is hidden in the comment is that it has already been executed.
-    # The features that were selected are embodied in extract_features,
-    # which only gives the selected features + the administrative features.
 
-    # chosen_vet_features = load_cache(
-    #     "vet_features.pkl",
-    #     lambda: vet_features(X_train, y_train),
-    #     force_recompute=recompute_functions.vet_features,
-    #     save=save_cache
-    # )
-    # X_train = X_train[chosen_vet_features]
-    # print('\033[32mFeature vetting completed\033[0m')
-
-    # #!TODO check normalization
+    chosen_vet_features = load_cache(
+        "vet_features.pkl",
+        lambda: vet_features(X_train, y_train),
+        force_recompute=recompute_functions.vet_features,
+        save=save_cache
+    )
+    X_train = X_train[chosen_vet_features]
+    print(chosen_vet_features)
+    print('\033[32mFeature vetting completed\033[0m')
 
     ## ========================================================================================================== ##
     ##                                               WINDOW MODELS                                                ##
@@ -116,7 +111,7 @@ def run_train(save_cache=False, recompute_functions=RecomputeFunctionsConfig(), 
         ## ==================================== Choose Best HP For Window Models ==================================== ##
         model_best_hp = load_cache(
             f"choose_hyperparameters_{window_model}.pkl",
-            lambda: choose_hyperparameters(X_selected, y_train, window_model, n_iterations=20, split_by_group_flag=True),
+            lambda: choose_hyperparameters(X_selected, y_train, window_model, n_iterations=50, split_by_group_flag=True),
             force_recompute=recompute_functions.choose_hyperparameters,
             save=save_cache
         )
@@ -174,7 +169,6 @@ def run_train(save_cache=False, recompute_functions=RecomputeFunctionsConfig(), 
     end_time = time.time()
     print(f"Total time: {end_time - start_time} sec")
 
-    return chosen_features
 
 # ========================================================= Run =========================================================
 # if __name__ == "__main__":
